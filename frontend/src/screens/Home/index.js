24f490a371bdd1f1635227309e42115dca796c3c
Catalin Rizea <catalinrizea1@gmail.com>
2020-08-08 20:48:40 +0300

    restoring after failed barcode test

--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -1,44 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
-import BarcodeScannerComponent from 'react-webcam-barcode-scanner';
-import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
+import useStylesCommon from '../stylesCommon';
 import messages from './messages';
-import InnerLayout from '../../components/InnerLayout';
-import { ROOT_PATH } from '../../navigation/routes';
 
 const Home = ({ intl: { formatMessage } }) => {
-  const [data, setData] = useState('Not Found');
-  const breadcrumbItems = [
-    {
-      key: 'home',
-      name: formatMessage(messages.title),
-      path: ROOT_PATH,
-    },
-  ];
-  return (
-    <InnerLayout
-      title={formatMessage(messages.title)}
-      breadcrumbItems={breadcrumbItems}>
-      <Grid item xs={12}>
-        <BarcodeScannerComponent
-          width={300}
-          height={300}
-          interval={200}
-          onUpdate={(err, result) => {
-            if (result) {
-              setData(result.text);
-            }
+  const classesCommon = useStylesCommon();
 
-            if (err) {
-              console.log(err);
-            }
-          }}
-        />
-        <p>{data}</p>
-      </Grid>
-    </InnerLayout>
+  return (
+    <div className={classesCommon.root}>
+      <Typography variant="h1">{formatMessage(messages.title)}</Typography>
+    </div>
   );
 };
 
