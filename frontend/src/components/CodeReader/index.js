caccf8daaefdbb3d4e956c0030011f2726d952df
Catalin Rizea <catalinrizea1@gmail.com>
2020-08-08 22:45:08 +0300

    added zxing multiformat code reader

---
+++ frontend/src/components/CodeReader/index.js
@@ -0,0 +1,147 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const style = {
+  width: '100%',
+  height: '100vh',
+  border: 'none',
+};
+
+const CodeReader = ({ handleRead }) => {
+  useEffect(() => {
+    const handler = (e) => {
+      if (e.origin !== process.env.REACT_APP_HOST_URL) {
+        return;
+      }
+
+      handleRead(e.data);
+    };
+
+    window.addEventListener('message', handler);
+
+    return () => window.removeEventListener('message', handler);
+  }, [handleRead]);
+
+  if (
+    !navigator.mediaDevices ||
+    typeof navigator.mediaDevices.enumerateDevices !== 'function'
+  ) {
+    return <p>Cannot use webcam to read codes...</p>;
+  }
+
+  return (
+    <div>
+      <iframe
+        style={style}
+        title="CodeReader"
+        srcDoc={`
+        <!doctype html>
+        <html lang="en">
+
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta name="author" content="ZXing for JS">
+
+          <title>ZXing Decoding from camera stream</title>
+
+          <link rel="stylesheet" rel="preload" as="style" onload="this.rel='stylesheet';this.onload=null"
+            href="/zxing/fonts.css">
+          <link rel="stylesheet" rel="preload" as="style" onload="this.rel='stylesheet';this.onload=null"
+            href="/zxing/normalize.css">
+          <link rel="stylesheet" rel="preload" as="style" onload="this.rel='stylesheet';this.onload=null"
+            href="/zxing/milligram.min.css">
+        </head>
+
+        <body>
+
+          <main class="wrapper" style="padding-top:0">
+
+            <section class="container" id="demo-content" style="margin:0; padding: 0;">
+              <div>
+                <a class="button" id="startButton">Start</a>
+                <a class="button" id="resetButton">Reset</a>
+              </div>
+
+              <div>
+                <video id="video" width="300" height="200" style="border: 1px solid gray"></video>
+              </div>
+
+              <div id="sourceSelectPanel" style="display:none">
+                <label for="sourceSelect">Change video source:</label>
+                <select id="sourceSelect" style="max-width:400px">
+                </select>
+              </div>
+
+              <label>Result:</label>
+              <pre><code id="result"></code></pre>
+            </section>
+          </main>
+
+          <script type="text/javascript" src="/zxing/library.min.js"></script>
+          <script type="text/javascript">
+            window.addEventListener('load', function () {
+              let selectedDeviceId;
+              const codeReader = new ZXing.BrowserMultiFormatReader()
+              console.log('ZXing code reader initialized')
+              codeReader.listVideoInputDevices()
+                .then((videoInputDevices) => {
+                  const sourceSelect = document.getElementById('sourceSelect')
+                  selectedDeviceId = videoInputDevices[0].deviceId
+                  if (videoInputDevices.length >= 1) {
+                    videoInputDevices.forEach((element) => {
+                      const sourceOption = document.createElement('option')
+                      sourceOption.text = element.label
+                      sourceOption.value = element.deviceId
+                      sourceSelect.appendChild(sourceOption)
+                    })
+
+                    sourceSelect.onchange = () => {
+                      selectedDeviceId = sourceSelect.value;
+                    };
+
+                    const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+                    sourceSelectPanel.style.display = 'block'
+                  }
+
+                  document.getElementById('startButton').addEventListener('click', () => {
+                    codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+                      if (result) {
+                        console.log(result)
+                        document.getElementById('result').textContent = result.text
+                      }
+                      if (err && !(err instanceof ZXing.NotFoundException)) {
+                        console.error(err)
+                        document.getElementById('result').textContent = err
+                      }
+                    })
+                    console.log('Started continous decode from camera with id' + selectedDeviceId);
+                  })
+
+                  document.getElementById('resetButton').addEventListener('click', () => {
+                    codeReader.reset()
+                    document.getElementById('result').textContent = '';
+                    console.log('Reset.')
+                  })
+
+                })
+                .catch((err) => {
+                  console.error(err)
+                })
+            })
+          </script>
+
+        </body>
+
+        </html>
+      `}
+      />
+    </div>
+  );
+};
+
+CodeReader.propTypes = {
+  handleRead: PropTypes.func.isRequired,
+};
+
+export default CodeReader;
